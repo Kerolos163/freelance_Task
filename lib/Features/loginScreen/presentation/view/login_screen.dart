fed42cb22dd092db3d5f02fa56e1caf477e0657e
kerolos <73710150+Kerolos163@users.noreply.github.com>
2024-01-13 17:29:04 +0200

    Add MultiProvider

--- lib/Features/loginScreen/presentation/view/login_screen.dart
+++ lib/Features/loginScreen/presentation/view/login_screen.dart
@@ -39,84 +39,81 @@ class _LogInScreenState extends State<LogInScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => LogInProvider(),
-      child: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  const Spacer(),
-                  TextFieldWithLabel(
-                    txt: AppStrings.phone,
-                    keyboardType: TextInputType.number,
-                    controller: phoneController,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter Phone Number';
-                      }
-                      return null;
-                    },
-                  ),
-                  const Gap(20),
-                  Consumer<LogInProvider>(
-                    builder: (context, myProvider, child) {
-                      return TextFieldWithLabel(
-                        txt: AppStrings.password,
-                        obscureText: !myProvider.isVisiable,
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            myProvider.changeVisibility();
-                          },
-                          child: Icon(
-                            myProvider.isVisiable
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                            color: AppColors.primary,
-                          ),
-                        ),
-                        controller: passwordController,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter Password';
-                          }
-                          return null;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Spacer(),
+                TextFieldWithLabel(
+                  txt: AppStrings.phone,
+                  keyboardType: TextInputType.number,
+                  controller: phoneController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter Phone Number';
+                    }
+                    return null;
+                  },
+                ),
+                const Gap(20),
+                Consumer<LogInProvider>(
+                  builder: (context, myProvider, child) {
+                    return TextFieldWithLabel(
+                      txt: AppStrings.password,
+                      obscureText: !myProvider.isVisiable,
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          myProvider.changeVisibility();
                         },
-                      );
-                    },
-                  ),
-                  const Spacer(),
-                  Consumer<LogInProvider>(
-                    builder: (context, myProvider, child) {
-                      return myProvider.state == LoginScreenState.loading
-                          ? const CircularProgressIndicator()
-                          : CustomButton(
-                              label: AppStrings.login,
-                              onPressed: () async {
-                                if (_formKey.currentState!.validate()) {
-                                  await myProvider.userLogin(
-                                    logInModelParam: LogInModelParam(
-                                      phone: phoneController.text,
-                                      password: passwordController.text,
-                                      phoneCode: "+20",
-                                    ),
-                                  );
-                                  if (myProvider.state ==
-                                      LoginScreenState.success) {
-                                    if (mounted) {
-                                      onTapLogin(myProvider, context);
-                                    }
+                        child: Icon(
+                          myProvider.isVisiable
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                          color: AppColors.primary,
+                        ),
+                      ),
+                      controller: passwordController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Password';
+                        }
+                        return null;
+                      },
+                    );
+                  },
+                ),
+                const Spacer(),
+                Consumer<LogInProvider>(
+                  builder: (context, myProvider, child) {
+                    return myProvider.state == LoginScreenState.loading
+                        ? const CircularProgressIndicator()
+                        : CustomButton(
+                            label: AppStrings.login,
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {
+                                await myProvider.userLogin(
+                                  logInModelParam: LogInModelParam(
+                                    phone: phoneController.text,
+                                    password: passwordController.text,
+                                    phoneCode: "+20",
+                                  ),
+                                );
+                                if (myProvider.state ==
+                                    LoginScreenState.success) {
+                                  if (mounted) {
+                                    onTapLogin(myProvider, context);
                                   }
                                 }
-                              },
-                            );
-                    },
-                  )
-                ],
-              ),
+                              }
+                            },
+                          );
+                  },
+                )
+              ],
             ),
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:freelance_task/Features/HomeScreen/presntation/view/home.dart';
 import 'package:freelance_task/Features/loginScreen/presentation/view/login_screen.dart';
+import 'package:freelance_task/Features/loginScreen/presentation/viewmodel/login_provider.dart';
 import 'package:freelance_task/core/api_services/dio_helper/dio_helper.dart';
 import 'package:freelance_task/core/cash_services/cashe_helper.dart';
 import 'package:freelance_task/core/constant.dart';
 import 'package:freelance_task/core/utils/app_colors.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,16 +25,23 @@ class MyApp extends StatelessWidget {
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-          primaryColor: AppColors.primary,
-          useMaterial3: true,
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider<LogInProvider>(
+            create: (context) => LogInProvider(),
+          )
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+            primaryColor: AppColors.primary,
+            useMaterial3: true,
+          ),
+          home: CasheHelper.getData(key: AppConstant.userToken) == null
+              ? const LogInScreen()
+              : const HomeScreen(),
         ),
-        home: CasheHelper.getData(key: AppConstant.userToken) == null
-            ? const LogInScreen()
-            : const HomeScreen(),
       ),
     );
   }
